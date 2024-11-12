09de49ca9223087cfba5adb06fa6988bb833cc0a
justinksw <kongshuwa@gmail.com>
2024-11-12 13:45:39 +0800

    test the test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("https://www.browserstack.com/guide/understanding-selenium-timeouts")
+    driver.get("http://example.com")
 
     st.code(driver.page_source)
 
